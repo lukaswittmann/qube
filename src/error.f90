07aaed34ef84c3b0921e597a0bea6b4a3c1a0d64
lukaswittmann <mail@lukaswittmann.com>
2023-08-17 12:20:35 +0200

    Added some comments.

--- src/error.f90
+++ src/error.f90
@@ -4,6 +4,12 @@ module qube_error
 
    contains
 
+   !> This subroutine raises an error or warning message and terminates the program.
+   ! Parameters:
+   !   mode: character, intent(in) :: mode - specifies the type of message to be raised. 
+   !          'w' for warning, 'e' for error, and any other character for unknown.
+   !   message: character(len=*), intent(in) :: message - the message to be raised.
+   ! Returns: None
    subroutine raise(mode,message)
       character,       intent(in) :: mode
       character(len=*),intent(in) :: message
@@ -18,6 +24,11 @@ module qube_error
       call terminate(1)
    end subroutine raise
 
+   !> This subroutine terminates the program based on the input signal.
+   ! If the signal is 0, the program stops normally.
+   ! If the signal is -1, the program stops with an error message indicating external termination.
+   ! If the signal is any other integer, the program stops with an error message.
+   ! @param signal The integer signal to determine how the program should terminate.
    subroutine terminate(signal)
       integer,intent(in) :: signal
       select case(signal)
@@ -30,15 +41,15 @@ module qube_error
       end select
    end subroutine terminate
 
+   !> Handles the SIGINT signal
+      subroutine wsigint
+         write(*,'(a)') '','Recieved sigint, terminating...'
+         call terminate(-1)
+      end subroutine wsigint
 
-   subroutine wsigint
-      print'(''Recieved sigint, terminating...'')'
-      call terminate(-1)
-   end subroutine wsigint
-
-
+   !> Handles the SIGTERM signal
    subroutine wsigterm
-      print'(''Recieved sigterm, terminating...'')'
+      write(*,'(a)') '','Recieved sigterm, terminating...'
       call terminate(-1)
    end subroutine wsigterm
 
